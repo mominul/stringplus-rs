37d1e5565e2ffd011fbdf6007d6e314b1be5a827
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-06-21 16:10:40 +0600

    Add StringPlus trait :tada:

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
+pub trait StringPlus {
+    fn substring(&self, start: usize, length: usize) -> &str;
+    fn at(&self, pos: usize) -> &str;
+}
+
+impl StringPlus for str {
+    fn substring(&self, start: usize, length: usize) -> &str {
+        &self[start..(start + length)]
+    }
+
+    fn at(&self, pos: usize) -> &str {
+        &self[pos..pos + 1]
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::StringPlus;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn substring() {
+        assert_eq!("Bangladesh".to_string().substring(0, 6), "Bangla");
+        assert_eq!("Bangladesh".substring(3, 4), "glad");
+    }
+
+    #[test]
+    fn at() {
+        assert_eq!("Hello".at(1), "e");
+        assert_eq!("Object".to_string().at(3), "e");
     }
 }
